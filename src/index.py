a13f70a396c8271b92aecd600b3c014a914b64e5
keitanamazue <keitanamazue@gmail.com>
2023-04-28 23:57:59 +0900

    fix header

--- src/index.py
+++ src/index.py
@@ -17,11 +17,12 @@ async def root():
     url_params = {"word": "PS4コントローラーレッド"}
     req_url = yodobashi_url + "/?" + urllib.parse.urlencode(url_params)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 Safari/537.36",
-        "Accept-Language": "ja,en-us;q=0.7,en;q=0.3",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
+        'Accept-Language': 'ja,en-us;q=0.7,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     }
+
 
     try:
         res = requests.get(req_url, headers=headers)
